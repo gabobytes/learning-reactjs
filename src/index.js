82f2ff509c94e39c27a744ba11609f242a1a689a
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2020-10-20 19:03:09 -0500

    first component

--- src/index.js
+++ src/index.js
@@ -1,36 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const user = {
-  firstName: 'Gabriel',
-  lastName: 'Melo',
-  avatar: 'https://icons.iconarchive.com/icons/icons8/windows-8/256/Cinema-Ninja-Turtle-icon.png'
-}
-
-function getName(user){
-  return `${user.firstName}  ${user.lastName}`
-}
-
-function getGreeting(user){
-  if(user)
-  {
-    return <h1>Hello {getName(user)}</h1>
-  }
-  return <h1>Hello Stranger</h1>
-
-}
-
-const name = 'Gabriel'
-const element =  (
-
-    <div>
-        <h1>{getGreeting(user) }</h1>
-        <img src={user.avatar} />
-    </div>
-)
-
+import Card from './components/Card'
 
 
 const container = document.getElementById('root')
 
-ReactDOM.render(element,container)
+ReactDOM.render(<Card/>,container)
