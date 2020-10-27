955734776449213adef1f6cd5f331e655262143b
Gabriel Melo <gabrielmeloarteaga@gmail.com>
2020-10-26 23:17:21 -0500

    routing dynamic components

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import imgButton from '../images/add.png'
+import {Link } from 'react-router-dom'
 
-function Button(props){
-    return(
+
+function Button(props) {
+    return (
         <div className="col-md-6 offset-md-5">
-                <div >
-                <img src={imgButton} />
-                </div>
+            <div >
+                <Link to="/exercise/new">
+                    <img src={imgButton} />
+                </Link>
+            </div>
         </div>
 
     )
